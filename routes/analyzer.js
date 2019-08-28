d95800fe3f57fbc59b4f40aa8a4c28389c0a8c6b
Dhiraj Sharma <dhiraj.sharma@viseo.com>
2019-08-28 18:02:00 +0800

    added api to get token

--- routes/analyzer.js
+++ routes/analyzer.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const AnalyzerController = require('../controllers/analyzer')
-
 const router = express.Router()
 
 router.get('/faces', AnalyzerController.getFacialAnalyzerDetails)
--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,7 @@ const express = require('express')
 const authController = require('../controllers/auth')
 const router = express.Router()
 
-router.post('/auth/token',(req,res,next)=>{
-console.log(req.body)
-next()
-}, authController.createToken)
+router.post('/auth/token', authController.createToken)
 
 
 module.exports = router
